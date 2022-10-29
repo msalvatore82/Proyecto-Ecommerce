f10e1d642f34bcf3be34bfb88b3e1c528cae0949
matias salvatore <[EMAIL]>
2022-10-29 22:33:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+// app.use("/clientes",require("./routes/clientes"))
+// app.use("/productos", require("./routes/productos"))
+// app.use("/categorias", require("./routes/categorias"))
+// app.use("/pedidos", require("./routes/pedidos"))
+// app.use("/cesta", require("./routes/cesta"))
+
+
+app.listen(PORT,()=>console.log(`Servidor levantado en el puerto ${PORT}`))
